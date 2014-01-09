7ae7e8d3fdfd9ecdbdf1d52b7929d3cf66537d77
Victor Nițu <victor@debian.org.ro>
2014-01-09 05:58:49 +0200

    Sampling before sleep(), so it will begin with the adjustments

--- autobrightness.py
+++ autobrightness.py
@@ -9,14 +9,14 @@ import time
 def convertToBmp(im_file):
 	img = Image.open(im_file)
 	img.save("out.bmp")
-	return 
+	return
 
 def brightness(im_file):
    im = Image.open(im_file)
    stat = ImageStat.Stat(im)
    r,g,b = stat.mean
    return math.sqrt(0.241*(r**2) + 0.691*(g**2) + 0.068*(b**2))
- 
+
 def camera():
 	import pygame.camera
 	pygame.camera.init()
@@ -29,9 +29,9 @@ def camera():
 	cam.stop()
 
 while True:
-	time.sleep(5)
-	camera()	
+	camera()
 	a = brightness('photo.bmp')
 	set = (a*100)/110
 	os.system('xbacklight -set '+str(set))
+	time.sleep(5)
 
