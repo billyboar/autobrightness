40383624a0850e12b9d339db5bbd647db04e8bf5
Victor Nițu <victor@debian.org.ro>
2014-01-09 06:25:05 +0200

    Fixed a bitmap location bug, exported interval as an optional shell argument

--- autobrightness.py
+++ autobrightness.py
@@ -4,12 +4,8 @@ import ImageStat
 import math
 import os
 import time
+import sys
 
-
-def convertToBmp(im_file):
-	img = Image.open(im_file)
-	img.save("out.bmp")
-	return
 
 def brightness(im_file):
    im = Image.open(im_file)
@@ -17,23 +13,24 @@ def brightness(im_file):
    r,g,b = stat.mean
    return math.sqrt(0.241*(r**2) + 0.691*(g**2) + 0.068*(b**2))
 
-def camera():
+def camera(tmpimg):
 	import pygame.camera
 	pygame.camera.init()
 	cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
 	cam.start()
 	img = cam.get_image()
-	tmpimg = "/tmp/autobrightness-sample.bmp"
 	import pygame.image
 	pygame.image.save(img, tmpimg)
 	pygame.camera.quit()
 	cam.stop()
-	os.remove(tmpimg)
 
 while True:
-	camera()
-	a = brightness('photo.bmp')
+	tmpimg = "/tmp/autobrightness-sample.bmp"
+	camera(tmpimg)
+	a = brightness(tmpimg)
+	os.remove(tmpimg)
 	set = (a*100)/110
 	os.system('xbacklight -set '+str(set))
-	time.sleep(5)
+	samplerate = float(sys.argv[1])
+	time.sleep(samplerate)
 
